ca3c28be04498993b87b1e4e36c0f60b238ae3ec
mukilukuna <mukilukuna@gmail.com>
2023-09-21 14:59:26 +0200

    commit

---
+++ src/components/CustomerList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Dashboard</h2>
+        <ul>
+            <li>Toegewezen rapportages</li>
+            <li>Kennisbase Documentatie</li>
+            <li>Instellingen</li>
+            <li>Login</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'CustomerList'
+    }
+</script>
+<style scoped>
+div{
+    text-align: left;
+}
+</style>
