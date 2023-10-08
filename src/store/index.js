faafcb47f3653e33f035daf51988c30ea2f02820
mukilukuna <mukilukuna@gmail.com>
2023-10-08 09:33:31 +0200

    AANPASSINGEN

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count: 0,
+    user: null,
   },
   getters: {
+    doubleCount: (state) => state.count * 2,
   },
   mutations: {
+    increment(state) {
+      state.count++
+    },
+    setUser(state, user) {
+      state.user = user
+    },
   },
   actions: {
+    fetchUser({ commit }) {
+      setTimeout(() => {
+        const user = { id: 1, name: 'John Doe' }
+        commit('setUser', user)
+      }, 1000)
+    },
   },
   modules: {
   }
